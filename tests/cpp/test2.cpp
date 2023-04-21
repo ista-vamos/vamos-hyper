4dd2cfc270d52d32662e09e1febc75f4d4478179
Marek Chalupa <mchqwerty@gmail.com>
2023-04-21 08:05:16 +0200

    update the test

--- tests/cpp/test2.cpp
+++ tests/cpp/test2.cpp
@@ -6,25 +6,65 @@
 
 using namespace vamos::hyper;
 
-struct Event_A : public vamos::Event {
-    int x;
-
-    Event_A() = default;
-    Event_A(int y) : x(y) {}
+enum class Kind : vms_kind {
+    A,
+    B,
 };
+
+struct TEvent : vamos::Event {
+    union {
+        struct {
+            int x;
+        } A;
+        struct {
+            float x;
+        } B;
+    } data;
+
+    TEvent() = default;
+    TEvent(Kind k, vms_eventid id) : Event((vms_kind)k, id) {}
+};
+
+struct Event_A : public TEvent {
+    Event_A() = default;
+    Event_A(vms_eventid id, int x): TEvent(Kind::A, id) {
+        data.A.x = x;
+    }
+
+    int x() const { return data.A.x; }
+};
+
+struct Event_B : public TEvent {
+    Event_B() = default;
+    Event_B(vms_eventid id, float x): TEvent(Kind::B, id) {
+        data.B.x = x;
+    }
+
+    float x() const { return data.B.x; }
+};
+
+
 
 int main() {
     TracesPipeline TP;
 
-    Trace *trace = new LocalTrace<Event_A>(TP);
-    for (int x = 1; x < 1000; ++x) {
-        trace->push(Event_A(x));
+    Trace *trace = new LocalTrace<TEvent>(TP);
+    for (int x = 1; x < 10; ++x) {
+        trace->push(Event_A(x, x));
+    }
+    for (int x = 10; x < 20; ++x) {
+        trace->push(Event_B(x, x+0.5));
     }
 
-    while (trace->has()) {
-        std::cout << static_cast<Event_A*>(trace->get())->x << "\n";
+    while (vamos::Event *e = trace->get()) {
+        if (e->kind() == (vms_kind)Kind::A) {
+          std::cout << static_cast<Event_A*>(e)->x() << "\n";
+        } else if (e->kind() == (vms_kind)Kind::B) {
+          std::cout << static_cast<Event_B*>(e)->x() << "\n";
+        }
         trace->consume();
     }
+    assert(!trace->has() && "Trace still has some events");
 
     delete trace;
 }
