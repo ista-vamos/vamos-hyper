a2d327e0fbd187f37e01564207eddab9663ecef5
Marek Chalupa <mchqwerty@gmail.com>
2023-06-06 21:22:22 +0200

    Add GPS monitor

---
+++ monitor-od/monitor_inputs_msr_gps.cpp
@@ -0,0 +1,90 @@
+#include <cassert>
+#include <fstream>
+
+#include "src-gps/monitor.h"
+#include "src-gps/events.h"
+
+int x;
+
+bool InputStream::hasEvent() const {
+  return !isDone();
+}
+
+bool InputStream::isDone() const {
+  size_t sending_output = reinterpret_cast<size_t>(data[2]);
+  return sending_output == 2;
+}
+
+Event *InputStream::getEvent() {
+  assert(hasEvent() && "getEvent() when there is no event");
+
+  std::ifstream *stream = reinterpret_cast<std::ifstream *>(data[0]);
+  std::string  date, time, pm;
+  float lat, lng;
+  static float llat, llng;
+
+  static Event_InputL  I(0, 0, 0);
+  static Event_OutputL O(0, 0, 0);
+  static Event_Other  oth(0, 0, 0);
+
+  size_t &pos = reinterpret_cast<size_t &>(data[1]);
+  size_t &sending = reinterpret_cast<size_t &>(data[2]);
+
+  assert(sending <= 1);
+  assert(stream->good());
+
+  if (!(*stream >> date >> time >> pm >> lat >> lng)) {
+      assert(sending <= 1);
+      sending = 2;
+      O = Event_OutputL(++pos, llat, llng);
+      return &O;
+  }
+
+  // std::cout << lat << " " << lng << "\n";
+
+  if (sending == 0) {
+      sending = 1;
+      I = Event_InputL(++pos, lat, lng);
+      return &I;
+  }
+
+  llat = lat;
+  llng = lng;
+  oth = Event_Other(++pos, lat, lng);
+  return &oth;
+}
+
+Inputs::Inputs(char **files) {
+  *reinterpret_cast<char ***>(&data[2]) = files;
+  assert(reinterpret_cast<char **>(data[2]) == files);
+}
+
+InputStream *Inputs::getNewInputStream() {
+  auto *files = reinterpret_cast<char **>(data[2]);
+  if (*files == nullptr)
+    return nullptr;
+
+  std::cout << "New stream: " << *files <<"\n";
+  auto *fstream = new std::ifstream(*files);
+  assert(fstream->good());
+  *reinterpret_cast<char ***>(&data[2]) = files + 1;
+
+  // skip the first line
+  std::string firstline;
+  std::getline(*fstream, firstline);
+
+  auto *stream = new InputStream(_streams.size());
+  _streams.emplace_back(stream);
+  *reinterpret_cast<std::ifstream **>(&stream->data[0]) = fstream;
+  reinterpret_cast<size_t&>(stream->data[1]) = 0;
+  *reinterpret_cast<size_t *>(&stream->data[2]) = 0;
+
+  return stream;
+}
+
+bool Inputs::done() const {
+  auto *files = reinterpret_cast<char **>(data[2]);
+  return *files == nullptr;
+}
+
+
---
+++ monitor-od/src-gps/events.h
@@ -0,0 +1,91 @@
+#ifndef OD_EVENTS_H_
+#define OD_EVENTS_H_
+
+#include <vamos-buffers/cpp/event.h>
+
+using vamos::Event;
+
+enum class Kind : vms_kind {
+  End = Event::doneKind(),
+  InputL = Event::firstValidKind(),
+  OutputL,
+  Other
+};
+
+inline const char *kindToStr(Kind k) {
+  switch (k) {
+  case Kind::InputL:
+    return "InputL";
+  case Kind::OutputL:
+    return "OutputL";
+  case Kind::Other:
+    return "Other";
+  case Kind::End:
+    return "END";
+  }
+}
+
+struct TraceEvent : Event {
+  union {
+    struct {
+      float lat;
+      float lng;
+    } InputL;
+    struct {
+      float lat;
+      float lng;
+    } OutputL;
+    struct {
+      float lat;
+      float lng;
+    } Other;
+  } data;
+
+  TraceEvent() = default;
+  TraceEvent(Kind k, vms_eventid id) : Event((vms_kind)k, id) {}
+  TraceEvent(vms_kind k, vms_eventid id) : Event(k, id) {}
+
+  bool operator==(const TraceEvent &rhs) const {
+    return kind() == rhs.kind() && (kind() == Event::doneKind() ||
+                                    (data.InputL.lat == rhs.data.InputL.lat &&
+                                     data.InputL.lng == rhs.data.InputL.lng));
+  }
+
+  bool operator!=(const TraceEvent &rhs) const { return !operator==(rhs); }
+};
+
+struct Event_InputL : public TraceEvent {
+  Event_InputL() = default;
+  Event_InputL(vms_eventid id, float lat, float lng)
+      : TraceEvent(Kind::InputL, id) {
+    data.InputL.lat = lat;
+    data.InputL.lng = lng;
+  }
+};
+
+struct Event_OutputL : public TraceEvent {
+  Event_OutputL() = default;
+  Event_OutputL(vms_eventid id, float lat, float lng)
+      : TraceEvent(Kind::OutputL, id) {
+    data.OutputL.lat = lat;
+    data.OutputL.lng = lng;
+  }
+};
+
+struct Event_Other : public TraceEvent {
+  Event_Other() = default;
+  Event_Other(vms_eventid id, float lat, float lng)
+      : TraceEvent(Kind::Other, id) {
+    data.Other.lat = lat;
+    data.Other.lng = lng;
+  }
+};
+
+
+#define DBG
+#ifdef DBG
+#include <iostream>
+std::ostream &operator<<(std::ostream &s, const TraceEvent &ev);
+#endif
+
+#endif
